0cb00d2acc8f561d4c05b7e06782a5dee5e9d461
Augusto <luzguga2010@gmail.com>
2024-10-27 13:00:03 -0300

    Adcionando schemas

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+from fast_zero.schemas import Message
 
 # http://127.0.0.1:8000/docs
 # http://127.0.0.1:8000/redoc
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message' : 'Olá Mundo!'}
